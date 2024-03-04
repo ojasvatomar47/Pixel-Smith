96245a5ac6da2909c423ab4ec7972ed5926e5ec6
Ojasva Tomar <ojasvatomar47@gmail.com>
2024-03-04 16:03:27 +0530

    Desktop and mobile sidebar setup

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
                 const isActive = link.route === pathname
 
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-400 text-white' : 'text-gray-700'
                     }`}>
                     <Link className="sidebar-link" href={link.route}>
                       <Image
@@ -47,7 +47,7 @@ const Sidebar = () => {
                 const isActive = link.route === pathname
 
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-400 text-white' : 'text-gray-700'
                     }`}>
                     <Link className="sidebar-link" href={link.route}>
                       <Image
@@ -70,7 +70,7 @@ const Sidebar = () => {
           </SignedIn>
 
           <SignedOut>
-            <Button asChild className="button bg-purple-gradient bg-cover">
+            <Button asChild className="button bg-purple-400 bg-cover">
               <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
